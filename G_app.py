0ef0cf3ff006ef4ebdb0c34a5a1cb989c2e4879d
David Gabriel <gdkimati@gmail.com>
2026-04-27 09:13:15 +0100

    Update G_app.py

--- G_app.py
+++ G_app.py
@@ -33,12 +33,18 @@ def work():
         Age_distance=AgeYears+DistancetoCBD
         Floor_parking_index=(0.5*(Floors+ParkingSpots)+SizeSqm)
         Comfort=Bedrooms/(Toilets+Bathrooms)
-        feature1=np.array([[State,PropertyType,Bedrooms,Bathrooms,Toilets,SizeSqm,Floors,AgeYears,ParkingSpots,SwimmingPool,Gym,Security,Furnished,Estate,YearBuilt,Total_str,Age_distance,Floor_parking_index,Comfort]])
+        feature1=np.array([[State,PropertyType,Bedrooms,Bathrooms,
+                            Toilets,SizeSqm,Floors,AgeYears,ParkingSpots,SwimmingPool,
+                            Gym,Security,Furnished,Estate,YearBuilt,Total_str,Age_distance,
+                            Floor_parking_index,Comfort]])
         price=(R_model.predict(feature1)[0]).round(1)
         Price_index=price/SizeSqm
         Price_Size=price/SizeSqm
         Price_INdex=(price+SizeSqm)/(Bathrooms+Bedrooms+Floors+Toilets)
-        feature2=np.array([[State,PropertyType,Bedrooms,Bathrooms,Toilets,SizeSqm,Floors,AgeYears,ParkingSpots,SwimmingPool,Gym,Security,Furnished,Estate,YearBuilt,Total_str,Price_index,Price_Size,Age_distance,Floor_parking_index,Price_INdex,Comfort]])
+        feature2=np.array([[State,PropertyType,Bedrooms,Bathrooms,Toilets,SizeSqm,Floors,
+                            AgeYears,ParkingSpots,SwimmingPool,Gym,Security,Furnished,Estate,
+                            YearBuilt,Total_str,Price_index,Price_Size,Age_distance,Floor_parking_index,
+                            Price_INdex,Comfort]])
         result=C_model.predict(feature2)[0]
         if result==2:
             category="Mid-Range"
